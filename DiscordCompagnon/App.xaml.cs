64516170025288cc2faa8a66318f35d1a2c4db66
Nathan HANEN <hanen.nathan@gmail.com>
2023-08-29 23:47:53 +0200

    app is single instance

--- DiscordCompagnon/App.xaml.cs
+++ DiscordCompagnon/App.xaml.cs
@@ -4,6 +4,7 @@ using System.Configuration;
 using System.Data;
 using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -14,8 +15,22 @@ namespace DiscordCompagnon
     /// </summary>
     public partial class App : Application
     {
+        private static Mutex? mutex;
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+            mutex?.ReleaseMutex();
+        }
+
         protected override void OnStartup(StartupEventArgs e)
         {
+            mutex = new Mutex(true, "{06749a37-1bcf-42aa-921d-7d7356e619b3}");
+            if (!mutex.WaitOne(TimeSpan.Zero, true))
+            {
+                mutex = null;
+                Environment.Exit(0);
+            }
             base.OnStartup(e);
         }
     }
